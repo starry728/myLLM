370d6dcdd64bf57170aba131a08356880c12c5ad
starry728 <18080621707@163.com>
2026-09-12 18:57:53 +0800

    rmsnorm

---
+++ method/rmsnorm.py
@@ -0,0 +1,13 @@
+import torch
+
+#开方求倒数
+t = torch.rsqrt(torch.tensor(4.0))
+print(t)
+
+#创建一个全1张量
+t2 = torch.ones(3,4)
+print(t2)
+
+#计算均值
+mean = t2.mean()
+print(mean)
--- model/model.py
+++ model/model.py
@@ -74,3 +74,22 @@ class MokioMindConfig(PretrainedConfig):
             if self.inference_rope_scaling
             else None
         )
+
+import torch
+import torch.nn as nn
+# 继承nn.Module类
+class RMSNorm(nn.Module):
+# __init__初始化
+    def __init__(self, dim:int, eps:float=1e-5):
+        super().__init__()
+        self.dim = dim
+        self.eps = eps
+        self.weight = nn.Parameter(torch.ones(dim))  # 初始化权重参数为全1张量
+
+# _norm
+    def _norm(self, x):
+        return torch.rsqrt(x.pow(2).mean(-1, keepdim=True)+self.eps)
+    
+# forward
+    def forward(self, x):
+        return self.weight * self._norm(x.float()).type_as(x) * x
