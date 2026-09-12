bab2bf6cc32a2d94b05127464492ab6db677cf75
starry728 <18080621707@163.com>
2026-09-12 21:00:18 +0800

    BLOCK

--- model/model.py
+++ model/model.py
@@ -289,3 +289,31 @@ class FeedForward(nn.Module):
     def forward(self,x):
         gated = self.act_fn(self.gate_proj(x)) * self.up_proj(x)
         return self.dropout(self.down_proj(self.act_fn(gated)))
+
+
+
+class MokioMindBlock(nn.Module):
+    def __init__(self,Layer_id:int,config:MokioMindConfig):
+        super().__init__()
+        self.num_attention_heads = config.num_attention_heads
+        self.hidden_size = config.hidden_size
+        self.head_dim = self.hidden_size//self.num_attention_heads
+        self.self_attn = Attention(config)
+
+        self.layer_id = Layer_id
+        self.input_layernorm = RMSNorm(config.hidden_size,eps=config.rms_norm_eps)
+        self.post_attention_layernorm = RMSNorm(config.hidden_size,eps=config.rms_norm_eps)
+        self.mlp = FeedForward(config)
+
+    def forward(self,hidden_states,position_embedding,past_key_value=None,use_cache=False,attention_mask=None):
+        residual = hidden_states
+        hidden_states,present_key_value = self.self_attn(
+            self.input_layernorm(hidden_states),
+            position_embedding,
+            past_key_value,
+            use_cache,
+            attention_mask
+        )
+        hidden_states = residual+hidden_states
+        hidden_states = hidden_states + self.mlp(self.post_attention_layernorm(hidden_states))
+        return hidden_states,present_key_value
